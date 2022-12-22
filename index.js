1c97fa322aa6b7f56e72c933a12ded4068b25d50
surajnakhale810 <surajnakhale810@gmail.com>
2022-12-22 12:04:26 +0530

    changes1

--- index.js
+++ index.js
@@ -3,61 +3,26 @@ let password = document.getElementById("password")
 let submit = document.getElementById("submit")
 let error = document.getElementById("error")
 let error1 = document.getElementById("error1")
+let sucess = document.getElementById("sucess")
 
 function loginFill(){
     let usernames = username.value 
     let passwords = password.value
-
-/*
-    if((usernames !== "") && usernames.includes("@") && usernames.includes(".")){
-        if((passwords !== "") && (passwords > 8)){
-           error1.innerText = "All Good To Go"
-           error1.style.color = "green"
-           error.innerText = ""
-       
-        }
-    }
     
+ if((usernames.length > 3) && (usernames.includes("@")) && (usernames.includes("."))&&(passwords.length > 8)){
+        error.innerText = ""
+        error1.innerText = ""
+        sucess.innerText = "All Good To Go"
+        sucess.style.color = "green"
+        return true 
+    }
     else{
-       error.innerText = "Make sure email is more than 3 characters and has @ and a . "
-        error1.innerText = "make sure password is more than 8 characters"
-        error1.style.color = "red"
-        error.style.color = "red"
-    }  */
-
-    if((usernames.length < 3) || (usernames == "")){
         error.innerText = "Make sure email is more than 3 characters and has @ and a . "
         error.style.color = "red"
-        
-    }
-    
-    else if((usernames.length > 3) && (usernames.includes("@")) && (usernames.includes("."))){
-        error.innerText = "" 
-        
-    }
-    // else{
-    //     error.innerText = ""
-    // }
 
-    // else if((usernames.length > 3) && (usernames.includes("@")) && (usernames.includes("."))){
-    //     error.innerText = ""
-    // }
-     if((passwords.length < 8)||(passwords == "")){
         error1.innerText = "Make sure password is more than 8 characters"
         error1.style.color = "red"
-        
-    }
-    else{
-        error1.innerText = "" 
-        
-    }
-    // else if(passwords.length > 8){
-    //     error1.innerText = ""
-    // }
-     if((usernames.length > 3) && (usernames.includes("@")) && (usernames.includes("."))&&(passwords.length > 8)){
-        error1.innerText = "All Good To Go"
-        error1.style.color = "green"
-        
+        return false
     }
     
     // return false
@@ -66,7 +31,16 @@ function loginFill(){
 // submit.onchange = loginFill
 
 function altCon(){
-    confirm("Are You Sure")
+    let validate = loginFill()
+    
+    if(validate == true){
+  let confirmation = confirm("are you sure, you don't have another chance")
+  if(confirmation == true){
     alert("Sucessful Signup!")
+  }
+  else{
+    location.reload()
+  }
+    }
 }
 submit.onclick = altCon
